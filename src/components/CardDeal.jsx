1073d00d41873718469520ebf9e336a3b11091c7
Sarthak Negi <sarthaknegi296@gmail.com>
2024-09-11 21:05:27 +0530

    bank-website

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,30 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal
+        <br className="sm:block hidden" />
+        in few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[480px] mt-5`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing. Error porro minus
+        repellendus tempora, vel natus! Impedit autem, voluptatum et unde quo
+        totam?
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img 
+        src={card}
+        alt="card"
+        className="w-[100%] h-[100%]"
+      />
+    </div>
+  </section>
+);
+
+export default CardDeal;
---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import NavBar from "./NavBar"
+import Billing from "./Billing"
+import Business from "./Business"
+import Button from "./Button"
+import CardDeal from "./CardDeal"
+import CTA from "./CTA"
+import Clients from "./Clients"
+import Feedback from "./Feedback"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import GetStarted from "./GetStarted"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+
+
+export {
+    NavBar,
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+    GetStarted,
+    Button,
+    Feedback
+}
